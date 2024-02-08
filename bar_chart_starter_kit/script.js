3fc142a8018fbd11e86e7b3dd2704d3c64d8f2da
Nina <nihi0001@kea.dk>
2024-02-08 09:40:33 +0100

    barchart

--- bar_chart_starter_kit/script.js
+++ bar_chart_starter_kit/script.js
@@ -13,8 +13,46 @@ function genererSoejle() {
     console.log("valueArr")
 
     const li = document.createElement("li");
-    li.style.setProperty("--height", "10");
+    li.style.setProperty("--height", Math.round(Math.random () * 100));
     list.appendChild(li);
 
     console.log("genererSoejle")
 }
+
+////
+
+const list = document.querySelector("ul");
+const arrCue = []
+setInterval(generateBrs, 1000);
+
+function generateBrs() {
+    console.log(arrCue);
+    const randoNum = getRandoNum();
+    arrCue.push.(randoNum);
+    const li = document.createElement("li");
+    li.style.setProperty("--height", randoNum);
+    list.appendChild(li);
+
+    if(arrCue.length>5){
+        arrCue.shift();
+        list.removeChild(list.firstChild);
+    }
+}
+
+function getRandoNum(){
+    return Math.round(Math.random()*100);
+    
+}
+
+
+    //arrCue.push(Math.round(Math.random () * 100));
+
+    //li.style.setProperty("--height", Math.round(Math.random () * 100));
+   // list.appendChild(li);
+
+    //console.log("genererSoejle")
+}
+
+
+
+
